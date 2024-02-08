733a1329f5fe3f3dafd8b9f8c8c8dfae3227d4a4
rbpolim <rbpolim@gmail.com>
2024-02-08 20:44:13 -0300

    chore: improve home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,33 @@
-import { useState } from "react";
-import { View, FlatList } from "react-native";
+import { Link } from "expo-router";
+import { useState, useRef } from "react";
+import { View, FlatList, SectionList, Text } from "react-native";
 
+import Product from "@/components/product";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
-import { CATEGORIES } from "@/utils/data/products";
+import { CATEGORIES, MENU } from "@/utils/data/products";
 
 export const Home = () => {
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  const sectionListRef = useRef<SectionList>(null);
 
   const onPress = (category: string) => {
     setCategory(category);
+
+    const sectionIndex = CATEGORIES.findIndex((item) => item === category);
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        sectionIndex,
+        itemIndex: 0,
+        animated: true,
+      });
+    }
   };
 
   return (
-    <View>
+    <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantity={7} />
       <FlatList
         data={CATEGORIES}
@@ -29,6 +43,24 @@ export const Home = () => {
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link asChild href={`/product/${item.id}`}>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-white text-xl font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
